a4b4c4340c38903f8f23321b8edfe81cf3030450
Helloyunho <yunho050840@gmail.com>
2018-05-12 22:33:26 +0900

    NOW CAN IMPORT NKPY FILESSSSSSS

--- nkpy/__init__.py
+++ nkpy/__init__.py
@@ -2,11 +2,14 @@ import sys
 import re
 import nkpy.vars
 import nkpy.utils
+import glob
+import atexit
+import os
 
 cmds = nkpy.vars.cmds
 
 
-def run(a):
+def run(a, excu=True):
     global cmds
 
     a = a.replace('큰따옴표', '"')
@@ -14,14 +17,43 @@ def run(a):
 
     cmds = nkpy.utils.sel_sort(cmds)
 
-    for m in cmds.values():
-        if a.find(m) != -1:
-            n = a.find(m)
-            raise SyntaxError('원래 있던 그런거는 쓰지 말아요. 이제 nkpy가 있으니깐요. {}에서 틀리셨습니다.'.format(a[n - 2] + a[n - 1] + a[n] + a[n + 1] + a[n + 2]))
+    k = re.findall(r'(로부터 (?P<from>[a-zA-Z0-9가-힣!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?.]*) )?가져와 (?P<import>[a-zA-Z0-9가-힣!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?.]*)( 으로 (?P<as>[a-zA-Z0-9가-힣!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?.]*))?', a)
+    k = list(k)
+    v = []
+    modules = []
+    for m in k:
+        for j in m:
+            if not j == '':
+                n = j.split('.')
+                for c in n:
+                    v.append(c)
+
+
+    for m in v:
+        j = glob.glob('{}.nkpy'.format(m), recursive=True)
+        for x in j:
+            modules.append(x)
+            with open('{}'.format(x), 'r') as b:
+                with open('{}'.format(x.replace('.nkpy', '.py')), 'w') as h:
+                    write_some = run(b.read(), excu=False)
+                    h.write(write_some)
 
     b = re.findall(r'\'(.+?)\'|"(.+?)"', a)
     for m in b:
         a = a.replace(m[0], '^')
+
+    for m in cmds.values():
+        bol = False
+        if a.find(m) != -1:
+            n = a.find(m)
+            for c in v:
+                h = len(c)
+                y = a.find(c)
+                for u in range(h):
+                    if a[y+u] == a[n]:
+                        bol = True
+            if not bol:
+                raise SyntaxError('원래 있던 그런거는 쓰지 말아요. 이제 nkpy가 있으니깐요. {}에서 틀리셨습니다.'.format(a[n - 2] + a[n - 1] + a[n] + a[n + 1] + a[n + 2]))
 
     for n in cmds.keys():
         a = a.replace(n, cmds[n])
@@ -29,7 +61,24 @@ def run(a):
     for m in b:
         a = a.replace('^', m[0], 1)
 
-    exec(a)
+    a = "import sys\nsys.path.insert(0, '{}')\n\n".format(os.getcwd()) + a
+
+    if excu:
+        try:
+            exec(a)
+            return modules
+        except KeyboardInterrupt:
+            return modules
+    else:
+        return a
+
+
+def on_exit(v):
+    try:
+        for a in v:
+            os.remove(a.replace('.nkpy', '.py'))
+    except NameError:
+        pass
 
 
 def rrun():
@@ -37,11 +86,16 @@ def rrun():
         try:
             with open(sys.argv[1], 'r') as v:
                 a = v.read()
-            run(a)
+            b = run(a)
+            atexit.register(on_exit, b)
         except FileNotFoundError:
             a = ' '.join(sys.argv)
             b = a.find(sys.argv[1])
             a = a[b:]
-            run(a)
+            c = run(a)
+            atexit.register(on_exit, c)
         except IndexError:
             exit()
+
+if __name__ == '__main__':
+    rrun()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='NKPy',
 
-      version='0.1.3',
+      version='0.1.20',
 
       url='https://github.com/NKPy/NKPy',
 
